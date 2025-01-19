8e4a86f6111575be1f61c9a83c11f7aa37b097aa
Drew101W <dwhiting9877@floridapoly.edu>
2025-01-19 02:16:03 +0000

    Square Input Program

---
+++ 04_get_input/get_input.c
@@ -0,0 +1,32 @@
+/*
+    Getting an input from the keyboard
+*/
+
+#include <stdio.h>
+
+int main() {
+    int square_side = 1;
+    int square_perimeter = 0;
+    int square_area = 0;
+
+    // Write the prompt
+    // puts - prints the string to the console 
+    puts("Enter the side of a square: ");
+
+    // Get the input
+    // & - address operator
+    // No comments, messages, or escape sequences in scanf
+    // Always add a & before every variable 
+
+    scanf("%d", &square_side);
+
+    // Do the math
+    square_perimeter = square_side * 4;
+    square_area = square_side * square_side;
+    
+
+    printf("The perimeter of a square with side %d is %d\n", square_side, square_perimeter);
+    printf("The area of a square with side %d is %d\n", square_side, square_area);
+
+    return 0;
+}
