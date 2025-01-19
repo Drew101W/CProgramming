fdc9321ae162f8ac8e1cfe82c32de31558b35237
Drew101W <dwhiting9877@floridapoly.edu>
2025-01-19 02:15:22 +0000

    Student Info Program

---
+++ 03_student_info/student_info.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int Course_ID = 2271;
+   int Student_ID = 9877;
+   int sum = Student_ID - Course_ID;
+   printf("Name: Drew Whiting\n");
+   printf("Course ID: %d\n", Course_ID);
+   printf("Student ID: %d\n", Student_ID);
+   printf("%d - %d = %d\n", Student_ID, Course_ID, sum);
+
+   return 0;
+}
